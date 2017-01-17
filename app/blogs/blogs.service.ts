62b6cea2d33a71616afa559523329219be472487
Voya <voya@voyacode.com>
2017-01-17 23:05:34 +0200

    Move blogs service + add imports

--- app/blogs/blogs.service.ts
+++ app/blogs/blogs.service.ts
@@ -15,18 +15,18 @@ export class BlogsService {
   // Limit can be used to limit how many are fetched. Limit 0 gets all blogs.
   getBlogs(limit: number = 0){
     return this.http.get(this.url + (limit ? '?limit=' + limit : '')).map((res: Response) =>{
-      console.log(res);
-      console.log(res.json());
       return res.json().data || [{
         id: 0,
         name: "Blog search failed.",
-        text: "Sorry, something went wrong. If this problem persists, report to the admin through comments page."
+        text: "Sorry, something went wrong. If this problem persists, report to the admin through comments page.",
+        date: ""
       }];
     }).catch(err => {
       return [[{
         id: 0,
         name: "Blog search failed (error: " + err + ").",
-        text: "Sorry, something went wrong and blog couldn't be fetched from the server. Check your internet connection and refresh the page.<br><br>If problem persists, report to the admin through comments page."
+        text: "Sorry, something went wrong and blog couldn't be fetched from the server. Check your internet connection and refresh the page.<br><br>If problem persists, report to the admin through comments page.",
+        date: ""
       }]];
     });
   }
