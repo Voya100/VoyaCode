db93c082939ebca2b4f9f77cace21cc65337e820
Voya <voya@voyacode.com>
2017-01-20 15:46:13 +0200

    Add 404 to routing

--- app/app.routing.js
+++ app/app.routing.js
@@ -7,7 +7,10 @@ var routes = [
     { path: 'projects', loadChildren: 'app/projects/projects.module#ProjectsModule' },
     { path: 'comments', loadChildren: 'app/comments/comments.module#CommentsModule' },
     // Project paths
-    { path: 'projects/other-projects', loadChildren: 'app/projects/other-projects/other-projects.module#OtherProjectsModule' }
+    { path: 'projects/other-projects', loadChildren: 'app/projects/other-projects/other-projects.module#OtherProjectsModule' },
+    // 404
+    { path: '404', loadChildren: 'app/not-found/not-found.module#NotFoundModule' },
+    { path: '**', redirectTo: '/404' }
 ];
 exports.routing = router_1.RouterModule.forRoot(routes);
 //# sourceMappingURL=app.routing.js.map
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -8,7 +8,10 @@ const routes: Routes = [
   { path: 'projects', loadChildren: 'app/projects/projects.module#ProjectsModule'},
   { path: 'comments', loadChildren: 'app/comments/comments.module#CommentsModule'},
   // Project paths
-  { path: 'projects/other-projects', loadChildren: 'app/projects/other-projects/other-projects.module#OtherProjectsModule'}
+  { path: 'projects/other-projects', loadChildren: 'app/projects/other-projects/other-projects.module#OtherProjectsModule'},
+  // 404
+  { path: '404', loadChildren: 'app/not-found/not-found.module#NotFoundModule'},
+  { path: '**', redirectTo: '/404'}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
