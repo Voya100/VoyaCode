e956f101abdc27bd08bf15b0593957352d9670a3
Voya <voya@voyacode.com>
2017-02-03 19:09:40 +0200

    Add rest of the project routes

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -10,7 +10,10 @@ const routes: Routes = [
   // Project paths
   { path: 'projects/other-projects', loadChildren: 'app/projects/other-projects/other-projects.module#OtherProjectsModule', data: {title: 'Other Projects'}},
   { path: 'projects/rock-paper-scissors', loadChildren: 'app/projects/rock-paper-scissors/rock-paper-scissors.module#RockPaperScissorsModule', data: {title: 'Rock, Paper, Scissors'}},
+  { path: 'projects/slay-the-dragon', loadChildren: 'app/projects/slay-the-dragon/slay-the-dragon.module#SlayTheDragonModule', data: {title: 'Slay the Dragon'}},
+  { path: 'projects/chess', loadChildren: 'app/projects/chess/chess.module#ChessModule', data: {title: 'Chess'}},
   { path: 'projects/snow-machine', loadChildren: 'app/projects/snow-machine/snow-machine.module#SnowMachineModule', data: {title: 'Snow Machine'}},
+  { path: 'projects/hangman', loadChildren: 'app/projects/hangman/hangman.module#HangmanModule', data: {title: 'Hangman'}},
   // 404
   { path: '404', loadChildren: 'app/not-found/not-found.module#NotFoundModule', data: {title: 'Page not found'}},
   { path: '**', redirectTo: '/404'}
