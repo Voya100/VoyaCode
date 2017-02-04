1cef47f6d810c542fbb9c7630ecdb63555a37cbb
Voya <voya@voyacode.com>
2017-02-04 23:34:17 +0200

    Remove files of old version

--- projects/snow/snow_app/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'my-app',
-  template: `<snow-machine></snow-machine>`
-})
-export class AppComponent  {  }
