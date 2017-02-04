4b68bc7c8714f4b9a4c8f732d9e6d5070655bd4b
Voya <voya@voyacode.com>
2017-02-04 20:19:02 +0200

    Move icons and banners to new folders

--- app/shared/data/projects.ts
+++ app/shared/data/projects.ts
@@ -12,36 +12,36 @@ export const projectList: Project[] = [
     name: 'Hangman',
     description: 'Hangman mobile app for Android.',
     path: 'projects/hangman',
-    bannerUrl: 'http://voyacode.com/images/hangman_banner.jpg',
-    iconUrl: 'http://voyacode.com/projects/icons/hangman_icon.jpg',
+    bannerUrl: '/images/banners/hangman-banner.jpg',
+    iconUrl: '/images/icons/hangman-icon.jpg',
     onBanner: true
   },{
     name: 'Snow Machine',
     description: 'Create all kinds of snow with Snow Machine!',
     path: 'projects/snow-machine',
-    bannerUrl: 'http://voyacode.com/images/snow_machine_banner.jpg',
-    iconUrl: 'http://voyacode.com/projects/icons/snow_machine_icon.jpg',
+    bannerUrl: '/images/banners/snow-machine-banner.jpg',
+    iconUrl: '/images/icons/snow-machine-icon.jpg',
     onBanner: true
   },{
     name: 'Chess',
     description: 'Play a game of chess against a friend or a computer AI!',
     path: 'projects/chess',
-    bannerUrl: 'http://voyacode.com/images/chess_banner.jpg',
-    iconUrl: 'http://voyacode.com/projects/icons/chess_icon.jpg',
+    bannerUrl: '/images/banners/chess-banner.jpg',
+    iconUrl: '/images/icons/chess-icon.jpg',
     onBanner: true
   },{
     name: 'Slay the Dragon!',
     description: 'Slay the dragon in the game "Slay the Dragon!"',
     path: 'projects/slay-the-dragon',
-    bannerUrl: 'http://voyacode.com/images/slay_the_dragon_banner.jpg',
-    iconUrl: 'http://voyacode.com/projects/icons/slay_the_dragon_icon.png',
+    bannerUrl: '/images/banners/slay-the-dragon-banner.jpg',
+    iconUrl: '/images/icons/slay-the-dragon-icon.jpg',
     onBanner: true
   },{
     name: 'Rock, Paper, Scissors',
     description: 'Beat the computer in a very challenging game of Rock, Paper, Scissors.',
     path: 'projects/rock-paper-scissors',
-    bannerUrl: 'http://voyacode.com/images/rock_paper_scissors_banner.jpg',
-    iconUrl: 'http://voyacode.com/projects/icons/rock_paper_scissors_icon.png',
+    bannerUrl: '/images/banners/rock-paper-scissors-banner.jpg',
+    iconUrl: '/images/icons/rock-paper-scissors-icon.jpg',
     onBanner: true
   },
 ];
