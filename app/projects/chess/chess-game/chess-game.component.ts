503cf9cede645c2e58ac5a8cbd8d27b93201b6f9
Voya <voya@voyacode.com>
2017-02-09 19:32:01 +0200

    Create chess game component and copy template

---
+++ app/projects/chess/chess-game/chess-game.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'chess-game',
+    templateUrl: 'chess-game.component.html',
+    styleUrls: ['chess-game.component.css']
+})
+export class ChessGameComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
--- app/projects/chess/chess.module.ts
+++ app/projects/chess/chess.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 
 import { ChessComponent }   from './chess.component';
+import { ChessGameComponent } from './chess-game/chess-game.component';
+import { ChessGameService } from './chess-game.service';
+
 import { routing } from './chess.routing';
 
 @NgModule({
   imports: [routing, SharedModule],
   exports: [],
-  declarations: [ChessComponent],
+  declarations: [ChessComponent, ChessGameComponent],
   providers: [],
 })
 export class ChessModule { }
