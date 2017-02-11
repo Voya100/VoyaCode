7dd6708b334989d73a310d1ec013234a51a44f0f
Voya <voya@voyacode.com>
2017-02-11 23:10:52 +0200

    Change lazy module paths to relative

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,12 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: 'app/home/home.module#HomeModule', pathMatch: 'full', data: {title: ''} },
-  { path: 'home', loadChildren: 'app/home/home.module#HomeModule', data: {title: 'Home'} },
-  { path: 'blogs', loadChildren: 'app/blogs/blogs.module#BlogsModule', data: {title: 'Blogs'}},
-  { path: 'projects', loadChildren: 'app/projects/projects.module#ProjectsModule', data: {title: 'Projects'}},
-  { path: 'comments', loadChildren: 'app/comments/comments.module#CommentsModule', data: {title: 'Comments'}},
-  { path: '404', loadChildren: 'app/not-found/not-found.module#NotFoundModule', data: {title: 'Page not found'}},
+  { path: '', loadChildren: './home/home.module#HomeModule', pathMatch: 'full', data: {title: ''} },
+  { path: 'home', loadChildren: './home/home.module#HomeModule', data: {title: 'Home'} },
+  { path: 'blogs', loadChildren: './blogs/blogs.module#BlogsModule', data: {title: 'Blogs'}},
+  { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule', data: {title: 'Projects'}},
+  { path: 'comments', loadChildren: './comments/comments.module#CommentsModule', data: {title: 'Comments'}},
+  { path: '404', loadChildren: './not-found/not-found.module#NotFoundModule', data: {title: 'Page not found'}},
   { path: '**', redirectTo: '/404'}
 ];
 
--- app/projects/projects.routing.ts
+++ app/projects/projects.routing.ts
@@ -5,12 +5,12 @@ import { ProjectsComponent } from './projects.component';
 
 const routes: Routes = [
   { path: '', component: ProjectsComponent },
-  { path: 'other-projects', loadChildren: 'app/projects/other-projects/other-projects.module#OtherProjectsModule', data: {title: 'Other Projects'}},
-  { path: 'rock-paper-scissors', loadChildren: 'app/projects/rock-paper-scissors/rock-paper-scissors.module#RockPaperScissorsModule', data: {title: 'Rock, Paper, Scissors'}},
-  { path: 'slay-the-dragon', loadChildren: 'app/projects/slay-the-dragon/slay-the-dragon.module#SlayTheDragonModule', data: {title: 'Slay the Dragon'}},
-  { path: 'chess', loadChildren: 'app/projects/chess/chess.module#ChessModule', data: {title: 'Chess'}},
-  { path: 'snow-machine', loadChildren: 'app/projects/snow-machine/snow-machine.module#SnowMachineModule', data: {title: 'Snow Machine'}},
-  { path: 'hangman', loadChildren: 'app/projects/hangman/hangman.module#HangmanModule', data: {title: 'Hangman'}},
+  { path: 'other-projects', loadChildren: './other-projects/other-projects.module#OtherProjectsModule', data: {title: 'Other Projects'}},
+  { path: 'rock-paper-scissors', loadChildren: './rock-paper-scissors/rock-paper-scissors.module#RockPaperScissorsModule', data: {title: 'Rock, Paper, Scissors'}},
+  { path: 'slay-the-dragon', loadChildren: './slay-the-dragon/slay-the-dragon.module#SlayTheDragonModule', data: {title: 'Slay the Dragon'}},
+  { path: 'chess', loadChildren: './chess/chess.module#ChessModule', data: {title: 'Chess'}},
+  { path: 'snow-machine', loadChildren: './snow-machine/snow-machine.module#SnowMachineModule', data: {title: 'Snow Machine'}},
+  { path: 'hangman', loadChildren: './hangman/hangman.module#HangmanModule', data: {title: 'Hangman'}},
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
