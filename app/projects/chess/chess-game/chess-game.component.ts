d437d5c9e8c40841527511e3e5132f41a85e6b19
Voya <voya@voyacode.com>
2017-02-15 17:53:45 +0200

    Remove module.id

--- app/projects/chess/chess-game/chess-game.component.ts
+++ app/projects/chess/chess-game/chess-game.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'chess-game',
     templateUrl: 'chess-game.component.html',
     styleUrls: ['chess-game.component.css']
