de073460edfe81bbcce6ad8bae59d7534fa74c43
Voya <voya@voyacode.com>
2017-02-19 00:27:37 +0200

    Add chess components + initial html setup

---
+++ app/projects/chess/chess-game/chess-board/chess-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'chess-board',
+    templateUrl: 'chess-board.component.html',
+    styleUrls: ['chess-board.component.css']
+})
+export class ChessBoardComponent {
+
+}
--- app/projects/chess/chess.module.ts
+++ app/projects/chess/chess.module.ts
@@ -7,11 +7,13 @@ import { ChessGameComponent } from './chess-game/chess-game.component';
 import { ChessGameService } from './chess-game.service';
 
 import { routing } from './chess.routing';
+import { ChessBoardComponent } from './chess-game/chess-board/chess-board.component';
+import { ChessInstructionsComponent } from './chess-game/chess-instructions/chess-instructions.component';
 
 @NgModule({
   imports: [routing, SharedModule],
   exports: [],
-  declarations: [ChessComponent, ChessGameComponent],
+  declarations: [ChessComponent, ChessGameComponent, ChessBoardComponent, ChessInstructionsComponent],
   providers: [],
 })
 export class ChessModule { }
