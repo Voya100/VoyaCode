3bfc49bc5fff08bb59af8da8a2bc64412b57dae1
Voya <voya@voyacode.com>
2017-05-13 12:02:51 +0300

    Remove old scrollbar from AppModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { MdProgressCircleModule } from '@angular/material';
-import { PerfectScrollbarModule, PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
  
-const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
-  wheelSpeed: 2.5
-};
 import { AppComponent }  from './app.component';
 
 import { MainFooterComponent } from './shared/main-footer/main-footer.component'
@@ -17,7 +13,7 @@ import { ScrollbarModule } from './shared/components/scrollbar/scrollbar.module'
 import { routing } from './app.routing';
  
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, HttpModule, MdProgressCircleModule, ScrollbarModule, PerfectScrollbarModule.forRoot(PERFECT_SCROLLBAR_CONFIG), routing ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, HttpModule, MdProgressCircleModule, ScrollbarModule, routing ],
   declarations: [ AppComponent, MainHeaderComponent, MainFooterComponent ],
   bootstrap:    [ AppComponent ]
 })
