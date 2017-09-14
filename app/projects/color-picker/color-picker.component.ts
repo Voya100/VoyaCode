9c05707056cdd90cec31cc8375ee0a2f7e6542ac
Voya <voya@voyacode.com>
2017-09-14 14:22:53 +0300

    Add color picker module and base component/routing

---
+++ app/projects/color-picker/color-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'color-picker.component.html',
+  styleUrls: ['./color-picker.component.scss']
+})
+export class ColorPickerComponent {
+  constructor() { }
+
+}
