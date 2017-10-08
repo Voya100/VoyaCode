0781146a13cb8a4a374b0fdd5f03f305a892f4ba
Voya <voya@voyacode.com>
2017-10-08 19:50:08 +0300

    Change unit test reporter to mocha

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
       noInfo: true
     },
 
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['mocha', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
