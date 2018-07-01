dfcb45929359f67bf24f7fabd73042c1fb3b2c3d
Voya <voya@voyacode.com>
2018-07-01 17:01:37 +0300

    Enable service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-
+import { routing } from './app.routing';
 import { AuthModule } from './authentication/auth.module';
 import { HomeModule } from './home/home.module';
 import { ScrollbarModule } from './shared/components/scrollbar/scrollbar.module';
 import { MainFooterComponent } from './shared/main-footer/main-footer.component';
 import { MainHeaderComponent } from './shared/main-header/main-header.component';
 import { SharedModule } from './shared/shared.module';
-
-import { routing } from './app.routing';
 
 @NgModule({
   imports: [
@@ -25,7 +26,8 @@ import { routing } from './app.routing';
     ScrollbarModule,
     SharedModule,
     HomeModule,
-    routing
+    routing,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [AppComponent, MainHeaderComponent, MainFooterComponent],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
@@ -10,4 +10,11 @@ if (environment.production) {
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  .then(() => {
+    // Service worker is registered here instead of AppModule because it doesn't work
+    // (https://github.com/angular/angular-cli/issues/8779)
+    if ('serviceWorker' in navigator && environment.production) {
+      navigator.serviceWorker.register('/ngsw-worker.js');
+    }
+  })
   .catch(err => console.log(err));
