351b1f3958c9c1300b3a363cd52aabd9f489ca06
Voya <voya@voyacode.com>
2018-08-02 00:08:12 +0300

    Add push subscription and unsubscription buttons

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -2,12 +2,22 @@ import { AfterViewChecked, ChangeDetectorRef, Component, OnInit, QueryList, View
 
 import { BlogPostComponent } from '../shared/components/blog-post/blog-post.component';
 import { BlogsService } from '../shared/services/blogs.service';
+import { PushService } from '../shared/services/push.service';
 
 import { Blog } from './blog';
 
 // Blogs page contains all blogs.
 // User can open/close blog contents either individually or all at once with buttons.
 // Blogs can be filtered by year.
+
+// State of (un)subscription request
+enum PushSubscriptionStateMessage {
+  NotStarted,
+  InProgress,
+  AlreadyRegistered,
+  Successful,
+  Failed
+}
 
 @Component({
   templateUrl: 'blogs.component.html',
@@ -16,17 +26,22 @@ import { Blog } from './blog';
 export class BlogsComponent implements OnInit, AfterViewChecked {
   @ViewChildren(BlogPostComponent) blogPosts: QueryList<BlogPostComponent>;
 
-  yearCheck: any = {};
+  yearCheck: { [key: number]: boolean } = {};
   blogs: Blog[] = [];
   years: any = [];
 
+  hasPushSubscription: boolean = false;
+  pushSubscriptionState = PushSubscriptionStateMessage.NotStarted;
+  pushSubscriptionStates = PushSubscriptionStateMessage;
+
   private initialised: boolean = false;
 
-  constructor(private blogsService: BlogsService, private cdRef: ChangeDetectorRef) {}
+  constructor(private blogsService: BlogsService, private pushService: PushService, private cdRef: ChangeDetectorRef) {}
 
   // Get blogs from BlogsService
   ngOnInit() {
     this.blogsService.getBlogs().subscribe(blogs => this.blogInit(blogs));
+    this.hasPushSubscription = this.pushService.isSubscribedToTopic('blogs');
   }
 
   // To prevent "expression has changed after it was checked" exception
@@ -64,5 +79,32 @@ export class BlogsComponent implements OnInit, AfterViewChecked {
   // Returns false if page has blog posts (that fit filters), otherwise true
   noBlogs() {
     return !this.initialised || this.blogPosts.length === 0;
+  }
+
+  async subscribeToPushNotifications() {
+    console.log('subscribe');
+    this.pushSubscriptionState = PushSubscriptionStateMessage.InProgress;
+    try {
+      await this.pushService.subscribeToBlogNotifications();
+      this.hasPushSubscription = this.pushService.isSubscribedToTopic('blogs');
+      this.pushSubscriptionState = PushSubscriptionStateMessage.Successful;
+    } catch (err) {
+      // TODO: Already registered message
+      console.warn(err);
+      this.pushSubscriptionState = PushSubscriptionStateMessage.Failed;
+    }
+  }
+
+  async unsubscribeFromPushNotifications() {
+    this.pushSubscriptionState = PushSubscriptionStateMessage.InProgress;
+    try {
+      await this.pushService.unsubscribeFromBlogNotifications();
+      this.hasPushSubscription = this.pushService.isSubscribedToTopic('blogs');
+      this.pushSubscriptionState = PushSubscriptionStateMessage.Successful;
+    } catch (err) {
+      // TODO: Already registered message
+      console.warn(err);
+      this.pushSubscriptionState = PushSubscriptionStateMessage.Failed;
+    }
   }
 }
