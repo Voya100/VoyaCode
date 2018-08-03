5160f55bfe22670d1e20a42acee5a14061aaf2b1
Voya <voya@voyacode.com>
2018-08-03 22:37:42 +0300

    In service worker change url to relative and check if openWindow is defined

--- src/sw-custom.js
+++ src/sw-custom.js
@@ -1,13 +1,14 @@
 // Voya Code service worker logic
 self.addEventListener('notificationclick', function (event) {
   const notification = event.notification;
-  const urlEnd = (notification.data && notification.data.url) || '';
-  const navigationUrl = 'https://voyacode.com' + urlEnd;
+  const navigationUrl = (notification.data && notification.data.url) || '';
   const availableActions = Notification.maxActions || 0;
 
   // If browser doesn't support actions, activate navigation on click
   if (availableActions === 0 || event.action === 'navigate') {
-    event.waitUntil(clients.openWindow(navigationUrl));
     notification.close();
+    if (clients.openWindow) {
+      event.waitUntil(clients.openWindow(navigationUrl));
+    }
   }
 });
